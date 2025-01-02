3036060c43ca0b6965975796c0044ee7d653d2ff
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-01-02 17:32:27 -0500

    Update V2.js

--- V2.js
+++ V2.js
@@ -46,7 +46,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 			title : {
 				media : {word : 'Positive'}, //Name of the category presented in the task.
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
+				height : 7 //Used to position the "Or" in the combined block. 
 			}, 
 			media : [ //Stimuli
 				{word: 'Paradise'},
